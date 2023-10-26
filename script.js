4048e50fbc095f83089dd6aaa7225d4f9d05615f
lacey aguigam <laceyaguigam@yahoo.com>
2023-10-25 18:55:44 -0700

    completed the class activity by using concole.log to solve the given expressions

--- script.js
+++ script.js
@@ -10,3 +10,18 @@
 // true || false = ???
 
 
+console.log(8 * null);
+
+console.log("5" - 1);
+
+console.log ("5" + 1);
+
+console.log("five" * 2);
+
+console.log (false == 0 );
+
+console.log( 5 > 2 & 6 > 3);
+
+console.log(false == false ? "equal" : "not equal");
+
+console.log (true || false);
