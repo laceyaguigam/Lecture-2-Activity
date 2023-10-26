1400ef32969a546d40f79eff25fc22b98204f83a
lacey aguigam <laceyaguigam@yahoo.com>
2023-10-25 18:09:14 -0700

    added the instructions and then commented them out

---
+++ script.js
@@ -0,0 +1,12 @@
+// use console.log(...) to write out the result of the following operations:
+
+// 8 * null = ???
+// “5” - 1 = ???
+// “5” + 1 = ???
+// “five” * 2 = ???
+// false == 0 = ???
+// 5 > 2 && 6 > 3 = ???
+// false == false ? “equal” : “not equal” = ???
+// true || false = ???
+
+
